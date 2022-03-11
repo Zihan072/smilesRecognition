f4d4380da4dc61adfc9b8627b01b64062039d9ef
Ngoc Quan Pham <quanpn90@gmail.com>
2022-03-11 19:39:52 +0100

    Rewrite decoder to avoid indexing 6 contiguous

--- model/model/Model.py
+++ model/model/Model.py
@@ -143,8 +143,8 @@ class MSTS:
 
                 targets = caps_sorted[:, 1:]
 
-                predictions.view(-1, predictions.size(-1))
-                targets = targets.view(-1)
+                predictions.contiguous().view(-1, predictions.size(-1))
+                targets = targets.contiguous().view(-1)
                 non_pad_mask = torch.nonzero(targets.ne(0)).squeeze(1)
                 targets = targets.index_select(0, non_pad_mask)
                 predictions = predictions.index_select(0, non_pad_mask)
@@ -225,8 +225,8 @@ class MSTS:
                 predictions, caps_sorted, decode_lengths, _, _ = self._decoder(imgs, sequence, sequence_lens)
                 targets = caps_sorted[:, 1:]
 
-                predictions.view(-1, predictions.size(-1))
-                targets = targets.view(-1)
+                predictions.contiguous().view(-1, predictions.size(-1))
+                targets = targets.contiguous().view(-1)
                 non_pad_mask = torch.nonzero(targets.ne(0)).squeeze(1)
                 targets = targets.index_select(0, non_pad_mask)
                 predictions = predictions.index_select(0, non_pad_mask)
